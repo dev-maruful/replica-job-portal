8d61efe6fa308d7b69aca0d87c4940ec36287225
Tawsif Adnan <tawsif.adnan22@gmail.com>
2023-08-13 22:52:47 +0600

    job details page design updated

--- src/app/jobDetails/[id]/page.jsx
+++ src/app/jobDetails/[id]/page.jsx
@@ -1,10 +1,13 @@
 "use client";
 
+import CarouselCard from "@/components/CarouselCard";
 import RelatedJobsCard from "@/components/RelatedJobsCard";
 import GetAllSellerJobs from "@/utils/getAllSellerJobs";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Carousel from "react-multi-carousel";
 import { ClockLoader } from "react-spinners";
+import "react-multi-carousel/lib/styles.css";
 
 const JobDetailsPage = () => {
   const router = usePathname();
@@ -30,7 +33,9 @@ const JobDetailsPage = () => {
   const relatedJobs = otherJobs?.filter(
     (job) => job.category === currentJob.category
   );
-  relatedJobs;
+  const otherJobsByThisSeller = allJobs.filter(
+    (job) => job.email === currentJob.email
+  );
 
   const {
     title,
@@ -44,77 +49,127 @@ const JobDetailsPage = () => {
     description,
   } = currentJob;
 
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      partialVisibilityGutter: 40,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
   return (
-    <div className="md:flex gap-10 mx-3 md:mx-0">
-      <div className="space-y-5 md:w-2/3 mb-10 md:mb-0">
+    <div className="mx-3 md:mx-0 space-y-10">
+      <div className="space-y-5 mb-10 md:mb-0">
         <h1 className="text-2xl font-medium">{title}</h1>
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-3">
+        <div className="flex gap-10">
+          <div className="md:w-2/3 space-y-5">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-3">
+                <img
+                  src={seller_image}
+                  alt="seller image"
+                  className="w-12 h-12 rounded-full object-cover"
+                />
+                <div>
+                  <h2 className="text-xl font-semibold">{seller_name}</h2>
+                  <h3 className="text-sm font-medium text-gray-500">
+                    {seller_title}
+                  </h3>
+                </div>
+              </div>
+            </div>
             <img
-              src={seller_image}
-              alt="seller image"
-              className="w-12 h-12 rounded-full object-cover"
+              src={photo}
+              alt="job photo"
+              className="w-full rounded-xl h-64 md:h-[480px] object-cover"
             />
-            <div>
-              <h2 className="text-xl font-semibold">{seller_name}</h2>
-              <h3 className="text-sm font-medium text-gray-500">
-                {seller_title}
-              </h3>
-            </div>
           </div>
-          <Link href={`/jobDetails/${jobId}/order`}>
-            <button className="bg-[#8c52ff] hover:bg-[#7A51CB] text-white font-bold py-2 px-4 rounded hidden md:block">
-              Order Now
-            </button>
-          </Link>
-        </div>
-        <img
-          src={photo}
-          alt="job photo"
-          className="w-full rounded-xl h-64 md:h-[500px] object-cover"
-        />
-        <div>
-          <h3 className="text-lg font-medium mb-5">Pricing:</h3>
-          <div className="flex gap-20">
+          <div className="md:w-1/3 space-y-5">
             <div>
-              <p className="font-medium">Basic</p>
-              <h2 className="text-2xl font-bold">${basic}</h2>
+              <h3 className="text-lg font-medium mb-3">Pricing:</h3>
+              <div className="flex gap-20">
+                <div>
+                  <p className="font-medium">Basic</p>
+                  <h2 className="text-2xl font-bold">${basic}</h2>
+                </div>
+                <div>
+                  <p className="font-medium">Standard</p>
+                  <h2 className="text-2xl font-bold">${standard}</h2>
+                </div>
+                <div>
+                  <p className="font-medium">Premium</p>
+                  <h2 className="text-2xl font-bold">${premium}</h2>
+                </div>
+              </div>
             </div>
             <div>
-              <p className="font-medium">Standard</p>
-              <h2 className="text-2xl font-bold">${standard}</h2>
+              <p className="text-lg font-medium mb-3">Description:</p>
+              <p className="text-gray-500">{description}</p>
             </div>
-            <div>
-              <p className="font-medium">Premium</p>
-              <h2 className="text-2xl font-bold">${premium}</h2>
-            </div>
+            <Link href={`/jobDetails/${jobId}/order`}>
+              <button className="bg-[#8c52ff] hover:bg-[#7A51CB] text-white font-bold py-2 px-4 rounded mt-5">
+                Order Now
+              </button>
+            </Link>
           </div>
         </div>
-        <div>
-          <p className="text-lg font-medium">Description:</p>
-          <p className="text-gray-500">{description}</p>
-        </div>
-        <Link href={`/jobDetails/${jobId}/order`}>
-          <button className="bg-[#8c52ff] hover:bg-[#7A51CB] text-white font-bold py-2 px-4 rounded mt-5">
-            Order Now
-          </button>
-        </Link>
       </div>
 
-      <div className="md:w-1/3">
+      {otherJobsByThisSeller.length !== 0 && (
+        <div>
+          <h1 className="text-xl font-medium border-b-2 border-black mb-5">
+            Other Jobs By This Seller
+          </h1>
+          <div>
+            <Carousel responsive={responsive} className="z-0">
+              {otherJobsByThisSeller.map((job, index) => (
+                <CarouselCard
+                  key={index}
+                  jobPhoto={job.photo}
+                  jobTitle={job.title}
+                  sellerImage={job.seller_image}
+                  sellerName={job.seller_name}
+                  sellerTitle={job.seller_title}
+                  pricing={job.basic}
+                ></CarouselCard>
+              ))}
+            </Carousel>
+          </div>
+        </div>
+      )}
+
+      <div>
         <h1 className="text-xl font-medium border-b-2 border-black mb-5">
-          Related Jobs
+          Related Jobs To This Category
         </h1>
         {relatedJobs.length !== 0 ? (
-          relatedJobs.map((job, index) => (
-            <RelatedJobsCard
-              key={index}
-              photo={job.photo}
-              title={job.title}
-              category={job.category}
-              price={job.basic}
-            ></RelatedJobsCard>
-          ))
+          <div>
+            <Carousel responsive={responsive} className="z-0">
+              {relatedJobs.map((job, index) => (
+                <CarouselCard
+                  key={index}
+                  jobPhoto={job.photo}
+                  jobTitle={job.title}
+                  sellerImage={job.seller_image}
+                  sellerName={job.seller_name}
+                  sellerTitle={job.seller_title}
+                  pricing={job.basic}
+                ></CarouselCard>
+              ))}
+            </Carousel>
+          </div>
         ) : (
           <p>No jobs related to this category</p>
         )}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -63,8 +63,6 @@ export default function Home() {
     },
   };
 
-  frontendJobs;
-
   return (
     <main className="space-y-10">
       {/* frontend carousel */}
